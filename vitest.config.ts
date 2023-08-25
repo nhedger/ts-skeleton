19d976236612010ffa2252a2c8b319513314ee95
Nicolas Hedger <nicolas@hedger.ch>
2023-08-25 12:22:15 +0200

    tests: enable coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		passWithNoTests: true,
+		coverage: {
+			reporter: ["text", "html"],
+		},
 	},
 	resolve: {
 		alias: {
